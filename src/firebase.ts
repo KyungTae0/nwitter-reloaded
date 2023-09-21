b0745819aed65732c2c4212b90b05685895d13c9
sam <ktra0@playauto.co.kr>
2023-09-21 18:09:01 +0900

    9. SETUP

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiU1Z-3oGaV34itp57PvecMtXm1kDSpaY",
+  authDomain: "nwitter-reloaded-db3c2.firebaseapp.com",
+  projectId: "nwitter-reloaded-db3c2",
+  storageBucket: "nwitter-reloaded-db3c2.appspot.com",
+  messagingSenderId: "1017384490651",
+  appId: "1:1017384490651:web:9ad61985d279c0c20c983e",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = initializeApp(firebaseConfig);
